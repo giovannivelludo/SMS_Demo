1990a1acbd5424ab242c170c479caaef8a547198
giovannivelludo <56405609+giovannivelludo@users.noreply.github.com>
2019-10-19 19:44:09 +0200

    Get destination number and message from EditText fields.

--- app/src/main/java/org/gruppo0/smsdemo/EditMessageActivity.java
+++ app/src/main/java/org/gruppo0/smsdemo/EditMessageActivity.java
@@ -3,11 +3,13 @@ package org.gruppo0.smsdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class EditMessageActivity extends AppCompatActivity {
 
-    public static final String EXTRA_MESSAGE = "org.gruppo0.smsdemo.MESSAGE";
-    public static final String EXTRA_NUMBER = "org.gruppo0.smsdemo.NUMBER";
+    //public static final String EXTRA_MESSAGE = "org.gruppo0.smsdemo.MESSAGE";
+    //public static final String EXTRA_NUMBER = "org.gruppo0.smsdemo.NUMBER";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +18,10 @@ public class EditMessageActivity extends AppCompatActivity {
     }
 
     /** Called when the user taps the SEND button */
-    public void sendMessage() {
-        // Do something
+    public void sendMessage(View view) {
+        EditText editText2 = (EditText) findViewById(R.id.editText2);
+        String message = editText2.getText().toString();
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String number = editText.getText().toString();
     }
 }
