aa471b8b3a61c2e0bfc10c0780b00dda73ee469c
giovannivelludo <56405609+giovannivelludo@users.noreply.github.com>
2019-10-20 15:37:49 +0200

    added SEND_SMS permission and ability to send SMS messages

--- app/src/main/java/org/gruppo0/smsdemo/EditMessageActivity.java
+++ app/src/main/java/org/gruppo0/smsdemo/EditMessageActivity.java
@@ -1,15 +1,30 @@
 package org.gruppo0.smsdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class EditMessageActivity extends AppCompatActivity {
 
     //public static final String EXTRA_MESSAGE = "org.gruppo0.smsdemo.MESSAGE";
     //public static final String EXTRA_NUMBER = "org.gruppo0.smsdemo.NUMBER";
+    public static final int SEND_SMS_FROM_EDITOR = 1;
+    private BroadcastReceiver onSend = null;
+    private BroadcastReceiver onDeliver = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +32,44 @@ public class EditMessageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_edit_message);
     }
 
-    /** Called when the user taps the SEND button */
+    // called when the user taps the SEND button
     public void sendMessage(View view) {
         EditText editText2 = (EditText) findViewById(R.id.editText2);
         String message = editText2.getText().toString();
         EditText editText = (EditText) findViewById(R.id.editText);
         String number = editText.getText().toString();
+
+        // check if SEND_SMS permission was granted
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+            // if not
+            ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.SEND_SMS}, SEND_SMS_FROM_EDITOR);
+        }
+        // yet to implement: ActivityCompat.OnRequestPermissionsResultCallback
+
+        SmsManager smsManager = SmsManager.getDefault();
+        PendingIntent sent = PendingIntent.getBroadcast(getApplicationContext(), 0, new Intent("SMS_SENT"), 0);
+        PendingIntent delivered = PendingIntent.getBroadcast(getApplicationContext(), 0, new Intent("SMS_DELIVERED"), 0);
+        onSend = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context arg0, Intent arg1) {
+                if (getResultCode()== Activity.RESULT_OK)
+                    Toast.makeText(arg0, "SMS sent", Toast.LENGTH_LONG).show();
+                else
+                    Toast.makeText(arg0, "Error while sending", Toast.LENGTH_LONG).show();
+            }
+        };
+        onDeliver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context arg0, Intent arg1) {
+                if (getResultCode()==Activity.RESULT_OK)
+                    Toast.makeText(arg0, "SMS delivered", Toast.LENGTH_LONG).show();
+                else
+                    Toast.makeText(arg0, "Error while delivering", Toast.LENGTH_LONG).show();
+            }
+        };
+        registerReceiver(onSend, new IntentFilter("SMS_SENT"));
+        registerReceiver(onDeliver, new IntentFilter("SMS_DELIVERED"));
+        smsManager.sendTextMessage(number, null, message, sent, delivered);
     }
 }
