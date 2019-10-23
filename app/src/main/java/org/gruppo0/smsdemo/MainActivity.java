663c91b0f18d59e7ee7a10e8483383f05ecbb31c
giovannivelludo <56405609+giovannivelludo@users.noreply.github.com>
2019-10-23 15:53:34 +0200

    first attempt at receiving SMS, not working because I can't start activity from SmsReceiver

--- app/src/main/java/org/gruppo0/smsdemo/MainActivity.java
+++ app/src/main/java/org/gruppo0/smsdemo/MainActivity.java
@@ -1,18 +1,33 @@
 package org.gruppo0.smsdemo;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
+
+    public static final int RECEIVE_SMS_FROM_BROADCAST = 2;
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == RECEIVE_SMS_FROM_BROADCAST && permissions[0].equals(Manifest.permission.RECEIVE_SMS))
+            if (grantResults[0] != PackageManager.PERMISSION_GRANTED)
+                //if permission to send SMS was granted, send a message
+                Toast.makeText(this, "You must grant permission to receive SMS", Toast.LENGTH_LONG).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +43,22 @@ public class MainActivity extends AppCompatActivity {
                 openEditor(view);
             }
         });
+
+        // check permission to receive SMS
+        // check if RECEIVE_SMS permission was granted
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED) {
+            // if not
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECEIVE_SMS}, RECEIVE_SMS_FROM_BROADCAST);
+        }
     }
+
+    /** at Override
+    protected void onResume() {
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(SmsReceiver.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
+    } */
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
